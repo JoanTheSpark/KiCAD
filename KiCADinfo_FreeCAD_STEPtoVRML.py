e88dbe86d338b435c1c9d6e3a5f59db5583527c5
Joan Spark <joanthesparky@gmail.com>
2016-01-08 01:14:27 +1000

    added VRML license feature
    the script now adds similar license details to the VRML files as it
    already does for the STEP files

--- KiCADinfo_FreeCAD_STEPtoVRML.py
+++ KiCADinfo_FreeCAD_STEPtoVRML.py
@@ -2,6 +2,8 @@
 features
 - change info in step file
 - import step, export vrml with scale applied
+- add license&info to vrml file
+- keep track of changes to step files to avoid redoing already done ones
 '''
 
 import sys, os, shutil
@@ -15,12 +17,11 @@ sys.path.append(PATH_FREECADBIN)
 import FreeCAD
 import Import
 import FreeCADGui
-#FreeCADGui.setupWithoutGUI()
 import ImportGui
 import Draft
 
-PATH_toStepFiles = "../Footprints/STEP/"
-PATH_toVrmlFiles = "../Footprints/VRML/"
+PATH_toStepFiles = "."
+PATH_toVrmlFiles = "."
 FNME_hashfile = "StepFileHashes.txt"
 
 STR_licAuthor = ""
@@ -28,7 +29,7 @@ STR_licEmail = ""
 STR_licOrgSys = ""
 STR_licPreProc = ""
 
-LIST_license = ["Copyright (C) 2015, " + STR_licAuthor,
+LIST_license = ["Copyright (C) "+datetime.now().strftime("%Y")+", " + STR_licAuthor,
                 "",
                 "This program is free software: you can redistribute it and/or modify",
                 "it under the terms of the GNU General Public License as published by",
@@ -43,7 +44,6 @@ LIST_license = ["Copyright (C) 2015, " + STR_licAuthor,
                 "You should have received a copy of the GNU General Public License",
                 "along with this program.  If not, see http://www.gnu.org/licenses/.",
                 ]
-
 
 
 def FNCT_modify_step(PMBL_stepfile,
@@ -113,6 +113,47 @@ def FNCT_modify_step(PMBL_stepfile,
     return(PMBL_modstepfile)
 
 
+def FNCT_modify_vrml(PMBL_vrmlfile,
+                     DICT_positions,
+                     LIST_license,
+                     FNME_vrmlfile,
+                     STR_licAuthor,
+                     STR_licEmail,
+                     STR_licOrgSys,
+                     STR_licPreProc,
+                     ):
+    """
+    DICT_positions:
+    V .. VRML line
+    G .. Group start
+    """
+    PMBL_modvrmlfile = []
+    STR_description = ""
+    STR_schema = ""
+    for line in range(DICT_positions["G"]):
+        if line < DICT_positions["V"]: # leave ISO/HEADER alone
+            PMBL_modvrmlfile.append(PMBL_vrmlfile[line].strip())
+        if line == DICT_positions["V"]: # add license
+            PMBL_modvrmlfile.append("")
+            PMBL_modvrmlfile.append("# LICENSE")
+            PMBL_modvrmlfile.append("# " + str(FNME_vrmlfile) + " 3D VRML model for use in ECAD systems")
+            for licline in LIST_license:
+                PMBL_modvrmlfile.append("# " + licline)
+            PMBL_modvrmlfile.append("#")
+            # DESCRIPTION
+            PMBL_modvrmlfile.append("# METADATA")
+            PMBL_modvrmlfile.append("# description 'model of " + str(FNME_vrmlfile) + "'")
+            PMBL_modvrmlfile.append("# filename '" + str(FNME_vrmlfile) + ".wrl'")
+            STR_TS = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+            PMBL_modvrmlfile.append("# time_stamp '" + STR_TS + "'")
+            PMBL_modvrmlfile.append("# author ('" + STR_licAuthor + "','" + STR_licEmail + "')"),
+            PMBL_modvrmlfile.append("# organization ('')")
+            PMBL_modvrmlfile.append("# preprocessor_version '" + STR_licPreProc + "'")
+            PMBL_modvrmlfile.append("# originating_system '" + STR_licOrgSys + "'")
+            PMBL_modvrmlfile.append("# authorisation ''")
+    PMBL_modvrmlfile.append("")
+    return(PMBL_modvrmlfile)
+    
 
 def FNCT_md5_for_file(FNME_file, NMBR_block_size=8192):
     md5 = hashlib.md5()
@@ -146,105 +187,157 @@ if __name__=='__main__':
     LIST_stepfiles = os.listdir(PATH_toStepFiles)
     LIST_stepfiles_clean = []
     for FNME_stepfile in LIST_stepfiles:
-        if os.path.isfile(PATH_toStepFiles + FNME_stepfile) == 1: #test if folder or file..
-            if FNME_stepfile[-3:] == "stp": #test if step file
-                if FNME_stepfile[-6:-4] != "_T": #test if step file
-                    LIST_stepfiles_clean.append(FNME_stepfile[:-4])
+        if os.path.isfile(PATH_toStepFiles + FNME_stepfile) == 1: # test if folder or file..
+            if FNME_stepfile[-3:] == "stp": # test if step file
+                FLAG_makeTransparent = False
+                if FNME_stepfile[-6:-4] != "_T": # test if transparency manipulation is needed
+                    FLAG_makeTransparent = True
+                LIST_stepfiles_clean.append(FNME_stepfile[:-4])
+                try:
+                    HDLR_stepfile = open(PATH_toStepFiles + FNME_stepfile, 'r') # open
+                except:
+                    print "broken_2"
+                STR_md5Hash = str(FNCT_md5_for_file(HDLR_stepfile))
+
+                #search for stepfile in old md5 hash list, compare hashes
+                FLAG_workFile = False
+                if FNME_stepfile[:-4] in DICT_md5Hashes.keys():
+                    if DICT_md5Hashes.get(FNME_stepfile[:-4]) != STR_md5Hash:
+                        FLAG_workFile = True #got changed step file here
+                else:
+                    FLAG_workFile = True #new step file here
+
+                if FLAG_workFile == True:
+                    print "working on",FNME_stepfile
+                    HDLR_stepfile.seek(0)
+                    PMBL_stepfile = HDLR_stepfile.readlines()
+                    HDLR_stepfile.close()
+                    CNT_cutoff = 0
+                    DICT_positions = {}
+                    for line in PMBL_stepfile:
+                        CNT_cutoff += 1
+                        if line[:6] == "HEADER":
+                            DICT_positions["H"] = CNT_cutoff
+                        elif line[:6] == "ENDSEC":
+                            DICT_positions["E"] = CNT_cutoff
+                        elif line[:4] == "DATA":
+                            DICT_positions["A"] = CNT_cutoff
+                            break
+                        if line[:5] == "FILE_":
+                            if line[5:9] == "DESC":
+                                DICT_positions["D"] = CNT_cutoff
+                            elif line[5:9] == "NAME":
+                                DICT_positions["N"] = CNT_cutoff
+                            elif line[5:9] == "SCHE":
+                                DICT_positions["S"] = CNT_cutoff
+
+                    LIST_PMBL = FNCT_modify_step(PMBL_stepfile[:DICT_positions["E"]],
+                                                 DICT_positions,
+                                                 LIST_license,
+                                                 FNME_stepfile[:-4],
+                                                 STR_licAuthor,
+                                                 STR_licEmail,
+                                                 STR_licOrgSys,
+                                                 STR_licPreProc,
+                                                 )
+
+                    # overwrite step file
                     try:
-                        HDLR_stepfile = open(PATH_toStepFiles + FNME_stepfile, 'r') # open
+                        HDLR_stepfile_w = open(PATH_toStepFiles + FNME_stepfile, 'w') # open
                     except:
-                        print "broken_2"
-                    STR_md5Hash = str(FNCT_md5_for_file(HDLR_stepfile))
-
-                    #search for stepfile in old md5 hash list, compare hashes
-                    FLAG_workFile = False
-                    if FNME_stepfile[:-4] in DICT_md5Hashes.keys():
-                        if DICT_md5Hashes.get(FNME_stepfile[:-4]) != STR_md5Hash:
-                            FLAG_workFile = True #got changed step file here
+                        print "broken_3"
                     else:
-                        FLAG_workFile = True #new step file here
+                        # overwrite with new preamble
+                        for line in LIST_PMBL:
+                            HDLR_stepfile_w.write(line + "\n")
+                        # add old data section
+                        for line in PMBL_stepfile[(DICT_positions["A"]-1):]:
+                            HDLR_stepfile_w.write(line.strip() + "\n")
+                        HDLR_stepfile_w.close()
 
-                    if FLAG_workFile == True:
-                        print "working on",FNME_stepfile[:-4]
-                        HDLR_stepfile.seek(0)
-                        PMBL_stepfile = HDLR_stepfile.readlines()
-                        HDLR_stepfile.close()
+                    # load STEP model, manipulate and save as VRML
+                    FreeCAD.newDocument("Unnamed")
+                    FreeCAD.setActiveDocument("Unnamed")
+                    ImportGui.insert(PATH_toStepFiles + FNME_stepfile,"Unnamed")
+                    FNME_vrmlfile = FNME_stepfile[:-4] + ".wrl"
+                    # change display mode and scale down
+                    for part in FreeCAD.ActiveDocument.Objects:
+                        part.ViewObject.DisplayMode = 1 #Shaded
+                        Draft.scale(part,delta=App.Vector(0.3937,0.3937,0.3937),center=App.Vector(0,0,0),legacy=True)
+                    if FLAG_makeTransparent: # if transparency is needed
+                        
+#                        from PySide import QtGui
+#                        QtGui.QInputDialog.getText(None, "Get text", "Input:")[0]
+                        pass
+                    __objs__=[] # select all parts (again)
+                    for part in FreeCAD.ActiveDocument.Objects:                    
+                        __objs__.append(part)
+                    FreeCADGui.export(__objs__,(PATH_toVrmlFiles + FNME_vrmlfile))
+                    del __objs__
+                    FreeCAD.closeDocument("Unnamed")
+
+                    
+                    # add license stuff to fresh VRML file
+                    print "working on",FNME_vrmlfile
+                    try:
+                        HDLR_vrmlfile = open(PATH_toVrmlFiles + FNME_vrmlfile, 'r') # open
+                    except:
+                        print "broken_4"
+                    else:
+                        HDLR_vrmlfile.seek(0)
+                        PMBL_vrmlfile = HDLR_vrmlfile.readlines()
+                        HDLR_vrmlfile.close()
                         CNT_cutoff = 0
                         DICT_positions = {}
-                        for line in PMBL_stepfile:
+                        for line in PMBL_vrmlfile:
                             CNT_cutoff += 1
-                            if line[:6] == "HEADER":
-                                DICT_positions["H"] = CNT_cutoff
-                            elif line[:6] == "ENDSEC":
-                                DICT_positions["E"] = CNT_cutoff
-                            elif line[:4] == "DATA":
-                                DICT_positions["A"] = CNT_cutoff
+                            if line[:15] == "#VRML V2.0 utf8":
+                                DICT_positions["V"] = CNT_cutoff
+                            elif line[:7] == "Group {":
+                                DICT_positions["G"] = CNT_cutoff
                                 break
-                            if line[:5] == "FILE_":
-                                if line[5:9] == "DESC":
-                                    DICT_positions["D"] = CNT_cutoff
-                                elif line[5:9] == "NAME":
-                                    DICT_positions["N"] = CNT_cutoff
-                                elif line[5:9] == "SCHE":
-                                    DICT_positions["S"] = CNT_cutoff
 
-                        LIST_PMBL = FNCT_modify_step(PMBL_stepfile[:DICT_positions["E"]],
+                        LIST_PMBL = FNCT_modify_vrml(PMBL_vrmlfile[:DICT_positions["G"]],
                                                      DICT_positions,
                                                      LIST_license,
-                                                     FNME_stepfile[:-4],
+                                                     FNME_vrmlfile[:-4],
                                                      STR_licAuthor,
                                                      STR_licEmail,
                                                      STR_licOrgSys,
                                                      STR_licPreProc,
                                                      )
 
-                        # overwrite step file
+                        # overwrite vrml file
                         try:
-                            HDLR_stepfile_w = open(PATH_toStepFiles + FNME_stepfile, 'w') # open
+                            HDLR_vrmlfile_w = open(PATH_toVrmlFiles + FNME_vrmlfile, 'w') # open
                         except:
-                            print "broken_3"
+                            print "broken_5"
                         else:
                             # overwrite with new preamble
                             for line in LIST_PMBL:
-                                HDLR_stepfile_w.write(line + "\n")
+                                HDLR_vrmlfile_w.write(line + "\n")
                             # add old data section
-                            for line in PMBL_stepfile[(DICT_positions["A"]-1):]:
-                                HDLR_stepfile_w.write(line.strip() + "\n")
-                            HDLR_stepfile_w.close()
+                            for line in PMBL_vrmlfile[(DICT_positions["G"]-1):]:
+                                HDLR_vrmlfile_w.write(line)
+                            HDLR_vrmlfile_w.close()
 
-                        # export scaled VRML
-                        FreeCAD.newDocument("Unnamed")
-                        FreeCAD.setActiveDocument("Unnamed")
-                        ImportGui.insert(PATH_toStepFiles + FNME_stepfile[:-4] + ".stp","Unnamed")
-                        __objs__=[]
-                        for part in FreeCAD.ActiveDocument.Objects:
-                            part.ViewObject.DisplayMode = 1 #Shaded
-                            Draft.scale(part,delta=App.Vector(0.3937,0.3937,0.3937),center=App.Vector(0,0,0),legacy=True)
-                            __objs__.append(part)
-                        FreeCADGui.export(__objs__,(PATH_toVrmlFiles + FNME_stepfile[:-4] + ".wrl"))
-                        del __objs__
-                        FreeCAD.closeDocument("Unnamed")
+                else:
+                    HDLR_stepfile.close()
 
-                        # add license stuff to fresh VRML file
-
-
-                    else:
-                        HDLR_stepfile.close()
-
-# rewrite hash file when done
+    # rewrite hash file when done
     if LIST_stepfiles_clean:
         LIST_stepfiles_clean.sort()
         try:
             HDLR_hashfile_w = open(PATH_toStepFiles + FNME_hashfile, 'w') # open
         except:
-            print "broken_4"
+            print "broken_6"
         else:
             print "writing new hashfile"
             for FNME_stepfile_clean in LIST_stepfiles_clean:
                 try:
                     HDLR_stepfile_r = open(PATH_toStepFiles + FNME_stepfile_clean + ".stp", 'r') # open
                 except:
-                    print "broken_5"
+                    print "broken_7"
                 else:
                     HDLR_hashfile_w.write(FNME_stepfile_clean + ";" + str(FNCT_md5_for_file(HDLR_stepfile_r)) + "\n")
                     HDLR_stepfile_r.close()
